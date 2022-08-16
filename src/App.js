0587d07dfb5850b6c89f82b085637205731fd034
gominsz <matheuscamargodealencar@gmail.com>
2022-08-16 15:48:23 -0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+
+const name = "Cabare do Gomin";
 
 function App() {
   return (
@@ -7,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>{name}</code>
         </p>
         <a
           className="App-link"
